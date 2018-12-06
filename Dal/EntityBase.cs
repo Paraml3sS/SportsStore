ecb1ea937aa6a18a583ed45043befc2e3a2a20c9
Yura Budzyn <yura.budz@gmail.com>
2018-12-06 22:42:09 +0200

    Start solution

---
+++ Dal/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dal
+{
+    public class EntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Timestamp]
+        public byte[] TimeStamp { get; set; }
+    }
+}
